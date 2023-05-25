5fcef25ade6c1eca0a1d18a3af30028dc0cca68e
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-05-25 08:32:31 +0800

    introducing computed properties

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Schawrzmuller';
+    }
+  },
   methods: {
     setName(event) {
       this.name = event.target.value + ' ' + lastName;
