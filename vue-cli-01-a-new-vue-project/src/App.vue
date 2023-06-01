6d25ec2fed86c5383032c474187e9465e6845883
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-01 11:47:51 +0800

    creating a basic  vue app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul v-for="friend in friends" :key="friend.id">
+      <li>{{ friend.name }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'e386dcac-343f-4bc4-9cf2-ba76dcc6739c',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 90',
+          email: "manuel@localhost.com"
+        },
+        {
+          id: '1efc6922-ec17-43ed-9d4a-6e4d602d67bd',
+          name: 'Julie Jones',
+          phone: '0987 654421 21',
+          email: 'julie@localhost.com'
+        }
+      ]
+    }
+  }
+}
+
+</script>
