27f74e7fdc09d2e173b20d48aaef1a20ebf5fc64
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-14 02:52:10 +0800

    demo adding components and connecting them

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="formSubmit">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName">
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="enteredEmail">
+    </div>
+    <div>
+      <button type="submit">Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: ''
+    }
+  },
+  methods: {
+    formSubmit() {
+      // const data = { name: this.enteredName, phone: this.enteredPhone, email: this.enteredEmail};
+      // console.log(data);
+      this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
