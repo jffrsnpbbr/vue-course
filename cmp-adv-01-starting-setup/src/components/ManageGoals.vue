ab48eeeb8723e73490a0bf3f1c5cb2b5c504e48b
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-20 21:05:44 +0800

    applying what we know and a problem

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,38 @@
 <template>
+  <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal"/>
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      console.log('enteredValue', enteredValue)
+      if (enteredValue == '') {
+        this.inputIsInvalid = true;
+      } else {
+        this.inputIsInvalid = false;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+}
+</script>
