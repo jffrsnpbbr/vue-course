03c08f0d1b2c37db9f09022a4f85f728646d9d89
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-22 00:07:08 +0800

    setup and first steps

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: crypto.randomUUID(),
+          title: 'Official Guide',
+          description: 'The official Vuejs Documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: crypto.randomUUID(),
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        },
+      ]
+    }
+  }
+}
+</script>
