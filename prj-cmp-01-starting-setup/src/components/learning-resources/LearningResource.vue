b26a0617ea53780601b2a37b89678c66861a9539
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-24 04:16:02 +0800

    Header and BaseCard Components

--- prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -1,17 +1,18 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header><h3>{{ title }}</h3></header>
       <button>Delete</button>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
 <script>
+
 export default {
   props: ['title', 'description', 'link']
 }
